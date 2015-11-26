13136e3cc3ff4401da446644c832c54bb124be95
Mahesh <mahesh.chandra61@gmail.com>
2015-11-26 17:00:29 +0530

    PUT/todos/:id

--- server.js
+++ server.js
@@ -63,6 +63,44 @@ app.delete('/todos/:id',function(req,res){
   
 });
 
+// UPDATE TODOS
+app.put('/todos/:id',function(req,res){
+  var todoId = parseInt(req.params.id,10);
+  var matchedTodo = _.findWhere(todos,{id:todoId});
+  
+  var body = _.pick( req.body ,'completed','description' );
+  
+  var validAttributes = {};
+  
+  if(!matchedTodo){
+    return res.status(404).send();
+  }
+  
+  if( body.hasOwnProperty('completed') && _.isBoolean(body.completed) ){
+    validAttributes.completed = body.completed;
+  }else if(body.hasOwnProperty('completed')){
+    return res.status(400).send();
+  }
+  
+  if( body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+    validAttributes.description = body.description.trim();
+  }
+  else if( body.hasOwnProperty('description') ){
+    return res.status(400).send();
+  }
+  
+  _.extend( matchedTodo , validAttributes );
+  res.json(matchedTodo);
+  
+});
+
 app.listen( PORT , function(){
   console.log('Server is running at Port : ' + PORT + '!');
 } );
+
+
+
+
+
+
+
