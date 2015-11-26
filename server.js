b6784027cb9ab53cb467ba642166946cbc550279
Mahesh <mahesh.chandra61@gmail.com>
2015-11-26 10:06:09 +0530

    added body-parser module and POST method for todos

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-  id:1,
-  description:'#1 . You have to report nodejs learning status today',
-  completed:false
-},{
-  id:2,
-  description:'#2 . Buy bread and butter today',
-  completed:false
-},{
-  id:3,
-  description:'#3 . Update Node.js Notes',
-  completed:true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get( '/' , function(req,res){
   res.send('Todo API root');
@@ -39,6 +32,16 @@ app.get('/todos/:id' , function(req,res){
   }
   
   res.send('You requested for item with id : ' + todoId);
+});
+
+// POST todos
+app.post('/todos',function(req,res){
+  var body = req.body;
+  
+  body.id = todoNextId++;
+  todos.push(body);
+  
+  res.json(body);
 });
 
 app.listen( PORT , function(){
