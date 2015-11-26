f9d53bbed0d4fdbf10b8c26c3ccdca8ca2d2d7de
Mahesh <mahesh.chandra61@gmail.com>
2015-11-26 14:28:32 +0530

    Underscore Module,pick,findWhere

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -19,12 +20,8 @@ app.get('/todos',function(req,res){
 
 app.get('/todos/:id' , function(req,res){
   todoId = parseInt(req.params.id,10);
-  var matchedTodo;
-  todos.forEach(function(todo){
-    if( todoId == todo.id ){
-      matchedTodo = todo;
-    } 
-  });
+  var matchedTodo = _.findWhere( todos , {id : todoId } );
+  
   if(matchedTodo){
     res.json(matchedTodo);
   }else{
@@ -37,7 +34,15 @@ app.get('/todos/:id' , function(req,res){
 
 // POST todos
 app.post('/todos',function(req,res){
-  var body = req.body;
+  var body = _.pick( req.body,'description','completed' );
+  
+  
+  
+  if( !_.isBoolean(body.completed ) || !_.isString(body.description) || body.description.trim() == "" ){
+    return res.status(400).send('Bad Data');
+  }
+  
+  body.description = body.description.trim();
   
   body.id = todoNextId++;
   todos.push(body);
